0490ff2a3d4d832518a91de320dbb55b4417e17f
Ana Tran <ana.tran323@gmail.com>
2023-05-19 19:08:47 -0500

    api in src

--- dist/api/index.js
+++ dist/api/index.js
@@ -5,4 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const apiRouter = express_1.default.Router();
+apiRouter.get('/', (req, res, next) => {
+    res.send({
+        message: "Server is online"
+    });
+});
+// routers
+const postsRouter = require('./posts');
+apiRouter.use('/posts', postsRouter);
+const usersRouter = require('./users');
+apiRouter.use('/users', usersRouter);
 module.exports = apiRouter;
---
+++ dist/src/api/api.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.APIURL = void 0;
+exports.APIURL = 'http://localhost:3000/api';
