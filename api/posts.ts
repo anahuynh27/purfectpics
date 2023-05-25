f649ef048de02caf2a63bcbb78fcf770c8af6645
vincent <vincentpalomo@yahoo.com>
2023-05-24 20:39:56 -0500

    implemented create post

--- api/posts.ts
+++ api/posts.ts
@@ -9,6 +9,27 @@ interface Post {
   content: string,
   userID: number,
   isActive: boolean,
-}
+};
+
+const {
+  createPost,
+} = require('../db/models/posts');
+
+// create post
+postsRouter.post('/create', async (req: Request, res: Response, next: NextFunction) => {
+  const { title, photo, content }: Post = req.body;
+
+  // validate title lenght
+  if (title.length < 1) {
+    return res.status(400).json({ message: 'Title cannot be empty' })
+  };
+
+  const post: object = await createPost({title, photo, content});
+
+  res.send({
+    message: `Post created successfully! 🐾`,
+    post
+  });
+});
 
 module.exports = postsRouter
--- dist/api/posts.js
+++ dist/api/posts.js
@@ -1,8 +1,33 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const postsRouter = express_1.default.Router();
+;
+const { createPost, } = require('../db/models/posts');
+// create post
+postsRouter.post('/create', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { title, photo, content } = req.body;
+    // validate title lenght
+    if (title.length < 1) {
+        return res.status(400).json({ message: 'Title cannot be empty' });
+    }
+    ;
+    const post = yield createPost({ title, photo, content });
+    res.send({
+        message: `Post created successfully! 🐾`,
+        post
+    });
+}));
 module.exports = postsRouter;
